5a463116b32127c492fd4b513056a455c6102125
Ignasio Dondo <ignasiodondo@standardbank.co.za>
2019-09-04 12:45:55 +0200

    Adjusting for newer npm library versions to lessen vulnerabilities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
+import { ConfirmationPageComponent } from './core/confirmation-page/confirmation-page.component';
+import { BeneficiariesService } from './services/beneficiaries.service';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { CoreModule } from './core/core.module';
 
 import { AppComponent } from './app.component';
+import { BeneficiariesListComponent } from './beneficiaries-list/beneficiaries-list.component';
+import { BeneficiaryFormComponent } from './beneficiary-form/beneficiary-form.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BeneficiariesListComponent,
+    BeneficiaryFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CoreModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    BeneficiariesService
+  ],
+  entryComponents: [
+    BeneficiaryFormComponent,
+    ConfirmationPageComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
